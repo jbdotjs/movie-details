b3265ef254bc1e883b7200060e07ab05f4f35606
JB Talha Khan <49081433+jbtalhakhan@users.noreply.github.com>
2020-02-11 18:42:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ import json, requests
 
 # get your free API key from here: http://omdbapi.com/
 
-movie_name=input('Enter movie name: ')
-
 api_key='YOUR API KEY'
-url=f"http://omdbapi.com/?apikey={api_key}&t={movie_name}"
 
-response=requests.get(url)
-movie_details=response.json()
+def movie_api(movie_name):
+	url=f"http://omdbapi.com/?apikey={api_key}&t={movie_name}"
 
-print('Movie Name:',movie_details['Title'])
-print('Released in:', movie_details['Released'])
-print('Genre:',movie_details['Genre'])
-print('Directed by:',movie_details['Director'])
-print('IMDB rating:',movie_details['imdbRating'])
-print('Plot:',movie_details['Plot'])
+	response=requests.get(url)
+	movie_details=response.json()
 
+	print('Movie Name:',movie_details['Title'])
+	print('Released in:', movie_details['Released'])
+	print('Genre:',movie_details['Genre'])
+	print('Directed by:',movie_details['Director'])
+	print('IMDB rating:',movie_details['imdbRating'])
+	print('Plot details:',movie_details['Plot'])
+
+
+movie_name=input('Enter movie name: ')
+movie_api(movie_name)
