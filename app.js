6c983c0af0240808a286559080758d61efee1168
JB Talha Khan <49081433+jbtalhakhan@users.noreply.github.com>
2020-08-26 06:13:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText) {
-    axios.get(`http://www.omdbapi.com/?s=${searchText}&apikey=64eee3bb`)
+    axios.get(`https://www.omdbapi.com/?s=${searchText}&apikey=64eee3bb`)
         .then((response) => {
             let movies = response.data.Search;
             let output = '';
